d4e4bcb3296788c3eb541e2a945db877198248c7
fergcb <fergus.bentley@gmail.com>
2022-01-05 20:15:54 +0000

    Tidy up generic paremeter extraction

--- src/computations/LiteralComputation.ts
+++ src/computations/LiteralComputation.ts
@@ -1,8 +1,10 @@
 import LiteralComputable, { BooleanComputable, NumberComputable, StringComputable } from 'src/computables/LiteralComputable'
 import Computation from './Computation'
 
+type TypeOf<T extends LiteralComputable<any>> = T extends LiteralComputable<infer K> ? K : never
+
 export default abstract class LiteralComputation<T extends LiteralComputable<any>> extends Computation<T> {
-  public evaluate (): T extends LiteralComputable<infer K> ? K : unknown {
+  public evaluate (): TypeOf<T> {
     return this.data.value
   }
 
